632939c11cc80bfa33b7740ad64e59dca2c37fc0
likejonathan <likejonathan@study.com>
2022-10-20 00:29:39 -0500

    optimizing Layout page default option highlighting

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,20 +1,12 @@
 
-import { LaptopOutlined, NotificationOutlined, UserOutlined, LogoutOutlined } from '@ant-design/icons';
+import { LogoutOutlined, HomeOutlined, DiffOutlined, EditOutlined } from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
 import React from 'react';
 import './home.css';
 
 const { Header, Content, Sider } = Layout
 
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-    const key = String(index + 1);
-    return {
-        key: `sub${key}`,
-        icon: React.createElement(icon),
-        label: `subnav ${key}`,
 
-    };
-});
 
 class Home extends React.Component {
 
@@ -29,21 +21,23 @@ class Home extends React.Component {
                 </div>
             </Header>
             <Layout>
-                <Sider width={200} className="site-layout-background">
+                <Sider width={200} >
                     <Menu
-                        theme='dark'
                         mode="inline"
+                        theme='dark'
+                        //This is for default highlight option
                         defaultSelectedKeys={['1']}
-                        // style={{
-                        //     height: '100%',
-                        //     borderRight: 0,
-                        // }}
-                        items={items2}
-                    />
+                    >
+                        <Menu.Item key="1" icon={<HomeOutlined />}>Data Overview</Menu.Item>
+                        <Menu.Item key="2" icon={<DiffOutlined />}>Article Management</Menu.Item>
+                        <Menu.Item key="3" icon={<EditOutlined />}>Article Publishment</Menu.Item>
+
+                    </Menu>
                 </Sider>
                 <Layout
                     style={{
-                        padding: '24px',
+                        //This can adjust content part padding
+                        padding: '14px',
                     }}
                 >
 
@@ -55,7 +49,7 @@ class Home extends React.Component {
                             minHeight: 280,
                         }}
                     >
-                        Content
+
                     </Content>
                 </Layout>
             </Layout>
